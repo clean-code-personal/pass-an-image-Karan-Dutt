34a244725e140a6d0287cc596272f020045c0ec1
Karan Dutt <karan.dutt@philips.com>
2024-04-11 15:25:49 +0530

    assignment

--- brightener.cpp
+++ brightener.cpp
@@ -1,32 +1,32 @@
 #include "brightener.h"
 
-ImageBrightener::ImageBrightener(Image& inputImage) : m_inputImage(inputImage) {
+ImageBrightener::ImageBrightener(const std::unique_ptr<Image>& inputImage) : m_inputImage(inputImage)
+{
 }
 
-int ImageBrightener::BrightenWholeImage() {
+int ImageBrightener::BrightenWholeImage() const {
 	// For brightening, we add a certain grayscale (25) to every pixel.
 	// While brightening, some pixels may cross the max brightness. They are
 	// called 'attenuated' pixels
 	int attenuatedPixelCount = 0;
-	for (int x = 0; x < m_inputImage.rows; x++) {
-		for (int y = 0; y < m_inputImage.columns; y++) {
-			if (m_inputImage.pixels[x * m_inputImage.columns + y] > (255 - 25)) {
+	for (int x = 0; x < m_inputImage->m_rows; x++) {
+		for (int y = 0; y < m_inputImage->m_columns; y++) 
+        {
+			if (m_inputImage->m_pixels[x * m_inputImage->m_columns + y] > (255 - 25)) 
+            {
 				++attenuatedPixelCount;
-				// m_inputImage.pixels[x * m_inputImage.rows + 1 + y] = 255;
-				m_inputImage.pixels[x * m_inputImage.columns + y] = 255;
+				m_inputImage->m_pixels[x * m_inputImage->m_columns + y] = 255;
 			}
-			else {
-				// --attenuatedPixelCount;
-				// m_inputImage.pixels[x * m_inputImage.columns + y] += 25;
-				int pixelIndex = x * m_inputImage.columns + y;
-				m_inputImage.pixels[pixelIndex] += 25;
-				// m_inputImage.pixels[x * m_inputImage.rows + 1 + y] += 25;
+			else 
+            {
+				int pixelIndex = x * m_inputImage->m_columns + y;
+				m_inputImage->m_pixels[pixelIndex] += 25;
 			}
 		}
 	}
 	return attenuatedPixelCount;
 }
 
-Image ImageBrightener::GetImage() {
+const std::unique_ptr<Image>& ImageBrightener::GetImage() const {
 	return m_inputImage;
 }
--- pass-an-image.cpp
+++ pass-an-image.cpp
@@ -2,11 +2,9 @@
 #include "brightener.h"
 
 int main() {
-    Image image;
-    image.rows = 512;
-    image.columns = 512;
+    const auto image = std::make_unique<Image>(512, 512);
 	std::cout << "Brightening a 512 x 512 image\n";
     ImageBrightener brightener(image);
-    int attenuatedCount = brightener.BrightenWholeImage();
+    const int attenuatedCount = brightener.BrightenWholeImage();
     std::cout << "Attenuated " << attenuatedCount << " pixels\n";
 }
